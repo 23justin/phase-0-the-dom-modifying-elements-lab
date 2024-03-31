c50cfae9bfe01c6adcc13f57da42dca6c2ddb78e
14justin <justinmwangi40@gmail.com>
2024-04-01 00:17:53 +0300

    test

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+let main ='h1'
+
+document.querySelector("main#main").remove();
+
+let newHeader =document.createElement('h1')
+newHeader.id ='victory'
+newHeader.innerHTML = ' Justin is the champion!';
+
+newHeader.class ='victory'
